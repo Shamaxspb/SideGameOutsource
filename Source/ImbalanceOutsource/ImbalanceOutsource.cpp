4f0a47581254474ac4cc7e91614f58c1a384197b
shamax <shamaxrus@outlook.com>
2023-07-11 18:42:08 +0300

    Added project

---
+++ Source/ImbalanceOutsource/ImbalanceOutsource.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ImbalanceOutsource.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ImbalanceOutsource, "ImbalanceOutsource" );
---
+++ Source/ImbalanceOutsource/ImbalanceOutsourceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ImbalanceOutsourceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMBALANCEOUTSOURCE_API AImbalanceOutsourceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
